f1c10b4105c1f9608b770835a973746a2d8221e0
pna6410 <pna6410@naver.com>
2022-11-24 10:41:14 +0900

    second commit

--- project_board4/src/main/java/egovframework/example/sample/service/SampleVO.java
+++ project_board4/src/main/java/egovframework/example/sample/service/SampleVO.java
@@ -1,36 +1,8 @@
-/*
- * Copyright 2008-2009 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package egovframework.example.sample.service;
 
 import java.sql.Date;
 
-/**
- * @Class Name : SampleVO.java
- * @Description : SampleVO Class
- * @Modification Information
- * @ @ 수정일 수정자 수정내용 @ --------- --------- ------------------------------- @
- *   2009.03.16 최초생성
- *
- * @author 개발프레임웍크 실행환경 개발팀
- * @since 2009. 03.16
- * @version 1.0
- * @see
- *
- *      Copyright (C) by MOPAS All right reserved.
- */
+
 public class SampleVO extends SampleDefaultVO {
 
 	private static final long serialVersionUID = 1L;
@@ -236,10 +208,8 @@ public class SampleVO extends SampleDefaultVO {
 	}
 	
 	
-	/////////////////////// ///////////////////////  
-
+	///////////////////////
 	
-
 	/** 아이디 */
 	private String id;
 
@@ -292,7 +262,7 @@ public class SampleVO extends SampleDefaultVO {
 	public void setRegUser(String regUser) {
 		this.regUser = regUser;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "code=" + code +", title=" +title+", name=" + name+", content=" + content + ", create_date = " + createDate;
